d419f0362d2769e42e09b9dc76e96b765934a896
Simon Danielsson <sd223ah.student.lnu.se>
2024-10-14 17:25:58 +0200

    Add test cases for proper card rank and card suit usage

---
+++ tests/cardRanks.test.js
@@ -0,0 +1,31 @@
+import { expect, test } from 'vitest'
+import { CardRank } from '../src/game/cardRanks.js'
+
+test('Assert strict usage of cardRanks', () => {
+  // Arrange
+  const rankTwo = CardRank.TWO
+  const rankFive = CardRank.FIVE
+  const rankQueen = CardRank.QUEEN
+  const rankACE = CardRank.ACE
+
+  // Assert
+  expect(rankTwo).toStrictEqual(CardRank.TWO)
+  expect(rankTwo).toBeInstanceOf(CardRank)
+  expect(rankTwo === 2).toBeFalsy()
+  expect(rankTwo.value).toStrictEqual(2)
+
+  expect(rankFive).toStrictEqual(CardRank.FIVE)
+  expect(rankFive).toBeInstanceOf(CardRank)
+  expect(rankFive === 5).toBeFalsy()
+  expect(rankFive.value).toStrictEqual(5)
+
+  expect(rankQueen).toStrictEqual(CardRank.QUEEN)
+  expect(rankQueen).toBeInstanceOf(CardRank)
+  expect(rankQueen === 10).toBeFalsy()
+  expect(rankQueen.value).toStrictEqual(10)
+
+  expect(rankACE).toStrictEqual(CardRank.ACE)
+  expect(rankACE).toBeInstanceOf(CardRank)
+  expect(rankACE === 11).toBeFalsy()
+  expect(rankACE.value).toStrictEqual(11)
+})
---
+++ tests/cardSuits.test.js
@@ -0,0 +1,31 @@
+import { expect, test } from 'vitest'
+import { CardSuit } from '../src/game/cardSuits.js'
+
+test('Assert strict usage of cardRanks', () => {
+  // Arrange
+  const suitHearts = CardSuit.HEARTS
+  const suitDiamonds = CardSuit.DIAMONDS
+  const suitClubs = CardSuit.CLUBS
+  const suitSpades = CardSuit.SPADES
+
+  // Assert
+  expect(suitHearts).toStrictEqual(CardSuit.HEARTS)
+  expect(suitHearts).toBeInstanceOf(CardSuit)
+  expect(suitHearts === 'hearts').toBeFalsy()
+  expect(suitHearts.value).toStrictEqual('hearts')
+
+  expect(suitDiamonds).toStrictEqual(CardSuit.DIAMONDS)
+  expect(suitDiamonds).toBeInstanceOf(CardSuit)
+  expect(suitDiamonds === 'diamonds').toBeFalsy()
+  expect(suitDiamonds.value).toStrictEqual('diamonds')
+
+  expect(suitClubs).toStrictEqual(CardSuit.CLUBS)
+  expect(suitClubs).toBeInstanceOf(CardSuit)
+  expect(suitClubs === 'clubs').toBeFalsy()
+  expect(suitClubs.value).toStrictEqual('clubs')
+
+  expect(suitSpades).toStrictEqual(CardSuit.SPADES)
+  expect(suitSpades).toBeInstanceOf(CardSuit)
+  expect(suitSpades === 'spades').toBeFalsy()
+  expect(suitSpades.value).toStrictEqual('spades')
+})
