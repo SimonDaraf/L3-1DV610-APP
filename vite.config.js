f1917acae5314f824196b1ce2ff38a2efb811369
Simon Danielsson <sd223ah.student.lnu.se>
2024-11-14 11:03:30 +0100

    Fix a majority of asset files not being included in vite build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,42 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig ({
   esbuild: {
     supported: {
       'top-level-await': true //browsers can handle top-level-await features
     },
-  }
+  },
+  build: {
+    lib: {
+      entry: 'src/index.js',
+      formats: ['es']
+    },
+    rollupOptions: {
+      output: {
+        format: 'es',
+        dir: 'dist',
+        preserveModules: true,
+        preserveModulesRoot: 'src'
+      }
+    }
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/**/*.html',
+          dest: 'assets'
+        },
+        {
+          src: 'src/**/*.css',
+          dest: 'assets',
+        },
+        {
+          src: 'src/**/*.png',
+          dest: 'assets',
+        },
+      ]
+    })
+  ]
 })
